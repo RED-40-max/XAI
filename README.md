31e6d547d8a19160b3332eb226b88fbf655b3f25
RED-40-max <roshinikitha@gmail.com>
2026-04-17 08:23:59 -0500

    inital set up

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # XAI
 stuff
+
+
+[0] HERO / HOOK (Full Screen) --> texting portion
+    [1] EXIGENCE + RESEARCH QUESTION
+
+[2] WHAT IS AN AI AGENT (Interactive Build)
+    [3] HOW AGENTS WORK (Sticky Diagram)
+[4] WHERE THINGS BREAK (Bug Simulation)
+
+[5] REAL-WORLD CONSEQUENCES
+[6] USER CONTROL / CALL TO ACTION
+[7] SOURCES + METHODOLOGY
---
+++ WRIT_P_3/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import LandingScene from "./components/LandingScene";
+import PhoneChat from "./components/PhoneChat";
+import Subtitle from "./components/Subtitle";
+import Blackout from "./components/Blackout";
+import TransitionSection from "./components/TransitionSection";
+import BlogSection from "./components/BlogSection";
+
+
+function App() {
+  return (
+    <div>
+      <h1>My Project</h1>
+    </div>
+
+
+  )
+}
+
+export default App
